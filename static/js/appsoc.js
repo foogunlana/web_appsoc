9d00f2696d4fbcac41ffb0163ef7444b1c4df27f
foogunlana <foogunlana@yahoo.com>
2015-02-03 19:16:18 +0000

    fixed jump in background image

--- static/js/appsoc.js
+++ static/js/appsoc.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$(window).resize("resizeBackground");
 		function resizeBackground(event) {
 			var screenHeight = $(window).height();
+			$('.events').height(screenHeight);
 			$(".appsoc_trans_view").height(screenHeight);
 		}
 		resizeBackground();
